d99d9717cb97827404e8e3d30a139a966e363a45
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-18 18:57:24 +0530

    hsvadhgvas

--- real-estate-app/app/(routes)/add-new-listing/page.jsx
+++ real-estate-app/app/(routes)/add-new-listing/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AddNewListing() {
     return (
-        <div className='mt-36 flex flex-col gap-5 justify-center items-center'>
+        <div className='flex flex-col gap-5 justify-center items-center'>
             <h2 className='text-2xl font-bold'>AddNewListing</h2>
             <div>
                 <h2 className='font-medium text-gray-400'>Enter Address which you want to add</h2>
