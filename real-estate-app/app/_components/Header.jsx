6d7d2476c5bfe2c2d7bc4fcce652c0b92d67dffc
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-18 14:34:24 +0530

    added

--- real-estate-app/app/_components/Header.jsx
+++ real-estate-app/app/_components/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
 
 function Header() {
     return (
-        <div>Header</div>
+        <div className='flex justify-between p-6 px-10 shadow-sm fixed top-0 w-full z-10 bg-slate-700'>
+            <div className='flex gap-12 items-center'>
+                <Image src="/logo.svg" alt="Dummy Logo" width={150} height={150} priority />
+                <ul className='hidden md:flex gap-10'>
+                    {/* // hidden for small screen */}
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100' > Home</li>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100' >Buy</li>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100' >Sell</li>
+                </ul>
+            </div>
+            <div className='flex gap-2'>
+                <Button><Plus className='h-4 w-4 mr-2' />Post Your Ad</Button>
+                <Button variant='outline'>Login</Button>
+            </div>
+        </div>
+
     )
 }
 
--- real-estate-app/tailwind.config.js
+++ real-estate-app/tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#39B3BA",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
