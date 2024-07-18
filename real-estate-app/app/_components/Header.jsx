650ce14fe7f2a038607ce8f68a37d70a3efac45f
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-18 14:40:19 +0530

    custom links

--- real-estate-app/app/_components/Header.jsx
+++ real-estate-app/app/_components/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 function Header() {
+    const path = usePathname();
+    useEffect(() => {
+        console.log(path);
+    }, [])
+
     return (
         <div className='flex justify-between p-6 px-10 shadow-sm fixed top-0 w-full z-10 bg-slate-700'>
             <div className='flex gap-12 items-center'>
                 <Image src="/logo.svg" alt="Dummy Logo" width={150} height={150} priority />
                 <ul className='hidden md:flex gap-10'>
                     {/* // hidden for small screen */}
-                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100' > Home</li>
-                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100' >Buy</li>
-                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100' >Sell</li>
+                    <Link href={'/'}><li className={`'hover:text-primary font-medium text-sm cursor-pointer text-slate-100' ${path === '/' ? 'text-primary' : 'text-slate-100'} `}> Buy</li></Link>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100'>Sell</li>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100'>Agent</li>
                 </ul>
             </div>
             <div className='flex gap-2'>
