7b6e06210813caecb61d148ddd58a412a42f445c
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-19 15:06:40 +0530

    ads

--- real-estate-app/app/(routes)/edit-listing/[id]/page.jsx
+++ real-estate-app/app/(routes)/edit-listing/[id]/page.jsx
@@ -1,5 +1,7 @@
 "use client"
 import React from 'react'
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 function EditListing() {
     return (
