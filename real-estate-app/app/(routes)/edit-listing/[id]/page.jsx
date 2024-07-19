5db1d564382ad8d7344f5323c1f3b1dc5da1f9c2
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-19 15:12:10 +0530

    added radio button

--- real-estate-app/app/(routes)/edit-listing/[id]/page.jsx
+++ real-estate-app/app/(routes)/edit-listing/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Label } from "../../../../components/ui/label"
+import { RadioGroup, RadioGroupItem } from "../../../../components/ui/radio-group"
 
 function EditListing() {
     return (
@@ -9,7 +9,19 @@ function EditListing() {
             <h2 className='text-xl font-bold'>Enter some more details about property</h2>
             <div className='p-8 rounded-lg shadow-md'>
                 <div className='grid grid-cols-1 md:grid-cols-3'>
-                    <h2>Rent or Sell</h2>
+                    <div className='flex flex-col gap-2'>
+                        <h2 className='text-lg font-medium text-slate-500'>Rent or Sell</h2>
+                        <RadioGroup defaultValue="Sell">
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="Rent" id="Rent" />
+                                <Label htmlFor="Rent">Rent</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="Sell" id="Sell" />
+                                <Label htmlFor="Sell">Sell</Label>
+                            </div>
+                        </RadioGroup>
+                    </div>
                 </div>
             </div>
         </div>
