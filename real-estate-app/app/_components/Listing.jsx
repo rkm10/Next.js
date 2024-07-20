838214f6229cae72dce53bb65e6eb6dcb05e1238
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-20 12:48:14 +0530

    anothersad

--- real-estate-app/app/_components/Listing.jsx
+++ real-estate-app/app/_components/Listing.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 function Listing({ latestListing }) {
     return (
         <div>
-            <div>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
                 {latestListing.map((item, index) => (
                     <div >
                         <Image src={item.listingImages[0].url}
                             width={800}
                             height={150}
-                            alt={item.name}
+                            // alt={item.name}
                             className='rounded-lg object-cover h-[150px]'
                         />
                     </div>
