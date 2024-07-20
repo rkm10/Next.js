86be9b6b85af9624ae0e6d96276b9b722f1a30c4
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-20 12:21:45 +0530

    new commit

--- real-estate-app/app/_components/Header.jsx
+++ real-estate-app/app/_components/Header.jsx
@@ -21,8 +21,8 @@ function Header() {
                 <Image src="/logo.svg" alt="Dummy Logo" width={150} height={150} priority />
                 <ul className='hidden md:flex gap-10'>
                     {/* // hidden for small screen */}
-                    <Link href={'/'}><li className={`'hover:text-primary font-medium text-sm cursor-pointer text-slate-100' ${path === '/' ? 'text-primary' : 'text-slate-100'} `}> Buy</li></Link>
-                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100'>Sell</li>
+                    <Link href={'/'}><li className={`'hover:text-primary font-medium text-sm cursor-pointer text-slate-100' ${path === '/' ? 'text-primary' : 'text-slate-100'} `}>For Sell</li></Link>
+                    <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100'>For Rent</li>
                     <li className='hover:text-primary font-medium text-sm cursor-pointer text-slate-100'>Agent</li>
                 </ul>
             </div>
--- real-estate-app/app/page.js
+++ real-estate-app/app/page.js
@@ -1,7 +1,8 @@
-
+import ListingMapView from './_components/ListingMapView';
 export default function Home() {
   return (
-    <>
-    </>
+    <div className='mt-20 p-10'>
+      <ListingMapView type='Sell' />
+    </div>
   );
 }
