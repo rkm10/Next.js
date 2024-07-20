d6ef60f944e16a72a70ae06e5939d72736d05e1b
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-20 12:40:12 +0530

    another

--- real-estate-app/app/_components/ListingMapView.jsx
+++ real-estate-app/app/_components/ListingMapView.jsx
@@ -1,9 +1,11 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Listing from './Listing'
-import supabase from '../../utils/supabase/client'
+import { supabase } from '../../utils/supabase/client'
+import { toast } from 'sonner'
 
 function ListingMapView({ type }) {
+    const [latestListing, setLatestListing] = useState([])
     useEffect(() => {
         getLatestListing();
     }, [])
@@ -11,17 +13,22 @@ function ListingMapView({ type }) {
     const getLatestListing = async () => {
         const { data, error } = await supabase
             .from('listing')
-            .select('*,listingImages(listing_id,url)')
+            .select(`*,listingImages(
+                listing_id,
+                url)`)
             .eq('active', true)
             .eq('type', type)
         if (data) {
-            console.log(data)
+            setLatestListing(data)
+        }
+        if (error) {
+            toast('server side error')
         }
     }
     return (
         <div className='grid grid-cols-1 md:grid-cols-2'>
             <div>
-                <Listing />
+                <Listing latestListing={latestListing} />
             </div>
             <div>
                 Map
