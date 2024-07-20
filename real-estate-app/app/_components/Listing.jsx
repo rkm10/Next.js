df354031625bc59e3eb71ffdd733ca58ef4ea79b
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-20 12:39:49 +0530

    1 push

--- real-estate-app/app/_components/Listing.jsx
+++ real-estate-app/app/_components/Listing.jsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
-function Listing() {
+function Listing({ latestListing }) {
     return (
-        <div>Listing</div>
+        <div>
+            <div>
+                {latestListing.map((item, index) => (
+                    <div >
+                        <Image src={item.listingImages[0].url}
+                            width={800}
+                            height={150}
+                            alt={item.name}
+                            className='rounded-lg object-cover h-[150px]'
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
