3848c2cc08af5505a78c318698ae8706ea73bf29
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-22 10:39:35 +0530

    ASD

--- real-estate-app/app/_components/FilterSection.jsx
+++ real-estate-app/app/_components/FilterSection.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+//Select shadcn
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '../../components/ui/select';
+import { BedDouble } from 'lucide-react';
 
 function FilterSection() {
     return (
-        <div>FilterSection</div>
+        <div className='px-3 py-2'>
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Bed" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="2"><h2 className='flex gap-2'> <BedDouble className='h-4 w-4 text-primary' />2+</h2></SelectItem>
+                    <SelectItem value="3"><h2 className='flex gap-2'> <BedDouble className='h-4 w-4 text-primary' />3+</h2></SelectItem>
+                    <SelectItem value="4"><h2 className='flex gap-2'> <BedDouble className='h-4 w-4 text-primary' />4+</h2></SelectItem>
+                    <SelectItem value="5"><h2 className='flex gap-2'> <BedDouble className='h-4 w-4 text-primary' />5+</h2></SelectItem>
+                </SelectContent>
+            </Select>
+
+        </div>
     )
 }
 
