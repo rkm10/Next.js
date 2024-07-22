27e68402e98ad8f7877d5577fb403522b435e979
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-22 13:50:52 +0530

    a commit

--- real-estate-app/app/_components/MarkerItem.jsx
+++ real-estate-app/app/_components/MarkerItem.jsx
@@ -1,12 +1,34 @@
-import { MarkerF } from '@react-google-maps/api'
-import React from 'react'
+import { MarkerF, OverlayView } from '@react-google-maps/api'
+import React, { useState } from 'react'
 
 function MarkerItem({ item }) {
+    const [selectedListing, setSelectedListing] = useState();
+
     return (
         <div>
             <MarkerF
                 position={item.coordinates}
+                onClick={() => setSelectedListing(item)}
+                icon={{
+                    url: `/house.svg`,
+                    scaledSize: {
+                        width: 60,
+                        height: 60
+                    }//new window.google.maps.Size(50, 50) , you use this as well
+                }}
             >
+                {selectedListing && <OverlayView
+                    position={selectedListing.coordinates}
+                    mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                >
+
+                    <div className='bg-white rounded-lg p-3'>
+                        <h2 className='text-xl font-bold'>{selectedListing.name}</h2>
+                        <h2 className=''>{selectedListing.address}</h2>
+                    </div>
+
+
+                </OverlayView>}
 
 
             </MarkerF>
