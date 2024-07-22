de3fb21acd9d7f0f92c186360a895f59ae7796c4
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-22 12:36:39 +0530

    1

--- real-estate-app/app/Provider.jsx
+++ real-estate-app/app/Provider.jsx
@@ -1,13 +1,20 @@
+'use client'
 import React from 'react'
 import Header from './_components/Header'
+import { LoadScript } from '@react-google-maps/api'
 
 function Provider({ children }) {
     return (
         <div>
+            {/* <LoadScript
+                googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+                libraries={["places"]}
+            > */}
             <Header />
-            <div >
+            <div className='mt-[90px]'>
                 {children}
             </div>
+            {/* </LoadScript> */}
         </div>
     )
 }
